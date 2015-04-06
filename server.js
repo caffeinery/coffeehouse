9c6b2a132f05b443e08e6007dc926360e5fe184b
Daniel Bugl <daniel.bugl@touchlay.com>
2015-04-06 20:46:28 +0200

    use normal coffea again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var coffea = require('../coffea/index');
+var coffea = require('coffea');
 
 app.use(express.static(__dirname + '/public'));
 
